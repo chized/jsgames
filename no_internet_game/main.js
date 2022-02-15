7f83cfe100ce5edee580de8ee2f11a9934f9f87e
chized <chiomaezedi@gmail.com>
2022-02-15 11:11:19 +0100

    1st commiy from PC

---
+++ no_internet_game/main.js
@@ -0,0 +1,15 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+function jump(){
+    if(character.classList != "animate"){
+    character.classList.add("animate");
+    }
+    setTimeout(function(){ 
+        character.classList.remove("animate");
+    },500);
+}
+var checkDead = setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+}, 10);
